c1e2742367eff44756036423ff93344f3d384c53
Chandrika0912 <chandrikamadupalli@gmail.com>
2024-09-11 18:17:30 +0530

    Add files via upload

--- home.js
+++ home.js
@@ -1,25 +1,88 @@
 $(document).ready(function() {
-    clickall();
-});
-function clickall(){
+    $("#accordionExample").hide();
+    $("#accordionExample1").hide();
+    $("#accordionExample2").hide();
+    $("#accordionExample3").hide();
+    $("#accordionExample4").hide();
     $('#btnusercreation').click(function() {
+        setActiveButton('#btnusercreation');
         // Toggle the accordion (show/hide)
-        $('#collapseOne').collapse('toggle');
+        $("#accordionExample").show();
+        $("#accordionExample1").hide();
+        $("#accordionExample2").hide();
+        $("#accordionExample3").hide();
+        $("#accordionExample4").hide();
+        $('#collapseOne').collapse('show');
     });
+
     $('#btnareacreation').click(function() {
+        setActiveButton('#btnareacreation');
         // Toggle the accordion (show/hide)
-        $('#collapseOnearea').collapse('toggle');
+        $("#accordionExample1").show();
+        $("#accordionExample2").hide();
+        $("#accordionExample3").hide();
+        $("#accordionExample4").hide();
+        $("#accordionExample").hide();
+        $('#collapseOnearea').collapse('show');
     });
+
     $('#btncustomtables').click(function() {
+        setActiveButton('#btncustomtables');
         // Toggle the accordion (show/hide)
-        $('#collapseOnecustomtables').collapse('toggle');
+        $("#accordionExample2").show();
+        $("#accordionExample3").hide();
+        $("#accordionExample4").hide();
+        $("#accordionExample").hide();
+        $("#accordionExample1").hide();
+        $('#collapseOnecustomtables').collapse('show');
     });
+
     $('#btnsqlquery').click(function() {
+        setActiveButton('#btnsqlquery');
         // Toggle the accordion (show/hide)
-        $('#collapseOnesqlquery').collapse('toggle');
+        $("#accordionExample3").show();
+        $("#accordionExample").hide();
+        $("#accordionExample1").hide();
+        $("#accordionExample2").hide();
+        $("#accordionExample4").hide();
+        $('#collapseOnesqlquery').collapse('show');
     });
+
     $('#btnmobileappversions').click(function() {
+        setActiveButton('#btnmobileappversions');
         // Toggle the accordion (show/hide)
-        $('#collapseOnemobileappversions').collapse('toggle');
+        $("#accordionExample4").show();
+        $("#accordionExample").hide();
+        $("#accordionExample1").hide();
+        $("#accordionExample2").hide();
+        $("#accordionExample3").hide();
+        $('#collapseOnemobileappversions').collapse('show');
     });
-}
+});
+
+    function setActiveButton(buttonId) {
+        // Remove 'active' class from all buttons
+        $('.creation').removeClass('active'); // Adjust selector to match all buttons you want to control
+        // Add 'active' class to the clicked button
+        $(buttonId).addClass('active');
+    }
+    // $('#btnusercreation').on('submit', function(event) {
+    //     event.preventDefault();
+    //     const UserID = $('#UserID').val();
+    //     const UserName = $('#UserName').val();
+    //     const user = {
+    //       UserID: '123',
+    //       UserName: 'Chandu'
+    //     };
+    //     if (username === user.username && password === user.password) {
+    //     } 
+    //     else {
+    //       alert('Invalid username or password');
+    //     }
+    //   });
+
+    
+
+    
+
+
